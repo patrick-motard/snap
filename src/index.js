5905883c47bfb2723188a4e3ed7ecb6dba12b86a
patrick-motard <motard19@gmail.com>
2018-03-24 20:06:23 -0500

    changed initial screen position and log keypresses
     ## Changed
    
     - screen now takes up full width, and sets height to a configurable portion of
     the screen height. Set dynamically based on screen resolution.
     - screen position now defaults to bottom of screen. Set dynamically based on
     screen resolution.

--- src/index.js
+++ src/index.js
@@ -12,29 +12,44 @@ const isDevMode = process.execPath.match(/[\\/]electron/);
 if (isDevMode) enableLiveReload();
 
 const createWindow = async () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-      transparent: true,
-      frame: false,
-      toolbar: false
-  });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+    var sr = require('screenres'),
+        screenHeight = sr.get()[1],
+        screenWidth = sr.get()[0],
+        appHeight = Math.round(screenHeight / 11),
+        appWidth = screenWidth,
+        yOffset = screenHeight - appHeight;
+    console.log(appHeight);
+    console.log(yOffset);
 
-  // Open the DevTools.
-  if (isDevMode) {
-    await installExtension(VUEJS_DEVTOOLS);
-    // mainWindow.webContents.openDevTools();
-  }
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        x: 0,
+        y: yOffset,
+        height: appHeight,
+        width: appWidth,
+        border: false,
+        transparent: true,
+        frame: false,
+        toolbar: false
+    });
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    // Open the DevTools.
+    if (isDevMode) {
+        await installExtension(VUEJS_DEVTOOLS);
+        // mainWindow.webContents.openDevTools();
+    }
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 };
 
 // This method will be called when Electron has finished
@@ -47,20 +62,29 @@ app.on('ready', async () => {
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
+
+
+app.on('web-contents-created', (event, wc) => {
+    wc.on('before-input-event', (event, input) => {
+        console.log(input);
+    })
+})
+
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
